60d9e989c893c0c058622616a200673bff9683b2
Márcio Abrantes <abs.marcio@gmail.com>
2018-09-13 18:01:10 -0300

    alterações

--- ApplicationSearch/app/src/main/java/com/marcioabrantes/applicationsearch/MainActivity.java
+++ ApplicationSearch/app/src/main/java/com/marcioabrantes/applicationsearch/MainActivity.java
@@ -10,12 +10,14 @@ import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
     private char[] spaces = {'&','3','2'};
+    private char[] spacesList = {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String v = getWithSpaces(new char[]{'U','s','e','r',' ', 'i','s',' ','n','o','t', ' ', 'a','l','l','o','w','e','d'});
+        String name = getName();
     }
 
     private String getWithSpaces(char[] description) {
@@ -43,22 +45,49 @@ public class MainActivity extends AppCompatActivity {
         return textFormed.toString();
     }
 
-    private String getName(char[] oringin,char[] generic){
+    private String getName(char[] oringin, char[] generic){
         char[] auxWeightOringin = new char[3];
         char[] auxWeightGeneric = new char[3];
-        int countWeight = 0;
+        measureWeight(oringin, generic, auxWeightOringin, auxWeightGeneric);
 
-        for (int i = 0, count = oringin.length; i < count; i++){
-            if(countWeight >= 3)
-                break;
-            if (oringin[i] != generic[i]){
-                auxWeightOringin[0] = oringin[i];
-                auxWeightGeneric[0] = generic[i];
-                countWeight++;
-            }
+        if(isValid(auxWeightOringin)){
+            return oringin.toString();
         }
 
         return null;
 
+    }
+
+    private void measureWeight(char[] oringin, char[] generic, char[] auxWeightOringin, char[] auxWeightGeneric){
+        int countWeight = 0;
+        for (int i = 0, count = oringin.length; i < count && countWeight >= 3; i++){
+            if (oringin[i] != generic[i]){
+                auxWeightOringin[countWeight] = oringin[i];
+                auxWeightGeneric[countWeight] = generic[i];
+                countWeight++;
+            }
+        }
+        validatWeight(auxWeightOringin, auxWeightGeneric);
+    }
+
+    private void validatWeight(char[] auxWeightOringin, char[] auxWeightGeneric){
+        for (int i = 0, count = auxWeightOringin.length; i < count; i++){
+            for (int j = 0, countG = auxWeightGeneric.length; j < countG; j++) {
+                if(auxWeightOringin[i] == auxWeightGeneric[j]){
+                    auxWeightOringin[i] = ' ';
+                    auxWeightGeneric[j] = ' ';
+                }
+            }
+        }
+    }
+
+    private boolean isValid(char[] auxOringin){
+        for (char aux:
+             auxOringin) {
+            if(aux != ' '){
+                return false;
+            }
+        }
+        return true;
     }
 }
