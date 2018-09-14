76ccb2b39776ec510ded1d12a5af13b09d5d50b7
Marcio Abrantes <abs.marcio@gmail.com>
2018-09-14 00:07:58 -0300

    mais

--- ApplicationSearch/app/src/main/java/com/marcioabrantes/applicationsearch/MainActivity.java
+++ ApplicationSearch/app/src/main/java/com/marcioabrantes/applicationsearch/MainActivity.java
@@ -1,23 +1,28 @@
 package com.marcioabrantes.applicationsearch;
 
-import android.content.SyncStatusObserver;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.ConsoleMessage;
-
-import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
     private char[] spaces = {'&','3','2'};
-    private char[] spacesList = {};
+            //you, yuo -> true
+            //probably, porbalby -> true
+            //despite, desptie -> true
+            //moon, nmoo -> false
+            //misspellings, mpeissngslli -> false
+    private String[] spacesListOringin = {"you", "probably", "despite", "moon", "misspellings"};
+    private String[] spacesListGeneric = {"yuo", "porbalby", "desptie", "nmoo", "mpeissngslli"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String v = getWithSpaces(new char[]{'U','s','e','r',' ', 'i','s',' ','n','o','t', ' ', 'a','l','l','o','w','e','d'});
-        String name = getName();
+        String name = "";
+        for (int i = 0, count = spacesListOringin.length; i < count; i++) {
+            name = getName(spacesListOringin[0].toCharArray(), spacesListGeneric[0].toCharArray());
+            System.out.println(name);
+        }
     }
 
     private String getWithSpaces(char[] description) {
@@ -46,8 +51,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String getName(char[] oringin, char[] generic){
+        if(oringin[0] != generic[0])
+            return null;
         char[] auxWeightOringin = new char[3];
         char[] auxWeightGeneric = new char[3];
+
         measureWeight(oringin, generic, auxWeightOringin, auxWeightGeneric);
 
         if(isValid(auxWeightOringin)){
@@ -60,12 +68,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void measureWeight(char[] oringin, char[] generic, char[] auxWeightOringin, char[] auxWeightGeneric){
         int countWeight = 0;
-        for (int i = 0, count = oringin.length; i < count && countWeight >= 3; i++){
+        for (int i = 0, count = oringin.length; i < count && countWeight <= 3; i++){
             if (oringin[i] != generic[i]){
                 auxWeightOringin[countWeight] = oringin[i];
                 auxWeightGeneric[countWeight] = generic[i];
                 countWeight++;
             }
+        }
+        if(countWeight >= 3){
+            return;
         }
         validatWeight(auxWeightOringin, auxWeightGeneric);
     }
